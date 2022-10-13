c62025dfb7d0a422d309032dba0931ae13635c60
Jared <jaredc.rob@gmail.com>
2022-10-13 11:54:15 -0600

    reverting to pre-pointer change

---
+++ proj1/cmake-build-debug/CMakeFiles/proj1.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/guest/j/jaredcr2/CS236/proj1/AddAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/AddAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/ColonAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/ColonAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/ColonDashAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/ColonDashAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/CommaAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/CommaAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/CommentAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/CommentAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/EndOfFileAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/EndOfFileAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/FactsAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/FactsAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/IdAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/IdAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/LeftParenAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/LeftParenAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/Lexer.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/Lexer.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/MultiplyAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/MultiplyAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/Parser.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/Parser.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/PeriodAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/PeriodAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/QmarkAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/QmarkAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/QueriesAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/QueriesAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/RightParenAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/RightParenAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/RulesAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/RulesAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/SchemesAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/SchemesAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/StringAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/StringAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/Token.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/Token.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/UndefinedAutomaton.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/UndefinedAutomaton.cpp.o"
+  "/users/guest/j/jaredcr2/CS236/proj1/main.cpp" "/users/guest/j/jaredcr2/CS236/proj1/cmake-build-debug/CMakeFiles/proj1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ proj1/cmake-build-debug/CMakeFiles/proj1.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj1.dir/AddAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/ColonAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/ColonDashAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/CommaAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/CommentAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/EndOfFileAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/FactsAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/IdAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/LeftParenAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/Lexer.cpp.o"
+  "CMakeFiles/proj1.dir/MultiplyAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/Parser.cpp.o"
+  "CMakeFiles/proj1.dir/PeriodAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/QmarkAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/QueriesAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/RightParenAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/RulesAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/SchemesAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/StringAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/Token.cpp.o"
+  "CMakeFiles/proj1.dir/UndefinedAutomaton.cpp.o"
+  "CMakeFiles/proj1.dir/main.cpp.o"
+  "proj1"
+  "proj1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
